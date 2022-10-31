0ba90368379f959630474abeffc37ee8c64d9dba
Iago Guinossi <iagoguinossi@gmail.com>
2022-10-31 18:59:55 -0300

    refactor

--- src/hooks/useIsMobile.js
+++ src/hooks/useIsMobile.js
@@ -2,10 +2,7 @@ import {useWindowSize} from './useWindowSize'
 
 export function useIsMobile(){
     const size = useWindowSize()
-    if(size.width < 820){
-        return true
-    }
-    return false
+    return size.width < 820
 }
 
 
