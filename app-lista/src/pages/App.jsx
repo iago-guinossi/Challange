5c2098c60e914e2ac4b7bfccd69e1c7a784c6d60
Iago Guinossi <iagoguinossi@gmail.com>
2022-10-31 14:17:26 -0300

    Add Header, ThemeProvider, Sizeprovider

--- app-lista/src/pages/App.jsx
+++ app-lista/src/pages/App.jsx
@@ -1,15 +1,20 @@
 import React from "react";
+import { ThemeProvider } from "../context/themeContext";
 import { TaskProvider } from "../context/taskContext";
 import { ViewProvider } from "../context/viewContext";
+import { SizeProvider } from "../context/sizeContext";
+import { Header } from "../components/Header/Header";
 import { NewTask } from "../components/NewTask";
 import { Task } from "../components/Task";
 import { FooterTask } from "../components/FooterTask/FooterTask";
 
 function App() {
   return (
+    <ThemeProvider>
+    <SizeProvider>
     <div className="container">
     <TaskProvider>
-      <div className='header'>TODO</div>
+      <Header/>
       <NewTask />
       <ViewProvider>
         <Task />
@@ -17,6 +22,8 @@ function App() {
       </ViewProvider>
     </TaskProvider>
     </div>
+    </SizeProvider>
+    </ThemeProvider>
   );
 }
 
