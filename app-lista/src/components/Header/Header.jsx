ce3eb6e7f400ff35812b3f77552b9a600d7365da
Iago Guinossi <iagoguinossi@gmail.com>
2022-10-31 17:41:55 -0300

    added mobile responsiveness

--- app-lista/src/components/Header/Header.jsx
+++ app-lista/src/components/Header/Header.jsx
@@ -1,26 +1,44 @@
 import React from "react";
 import "./Header.css";
 import { useTheme } from "../../context/themeContext";
+import { useIsMobile } from "../../hooks/useIsMobile";
 
 export function Header() {
+  const isMobile = useIsMobile();
   const [theme, setTheme] = useTheme("dark");
   let themeId = document.getElementById("theme-id");
   if (!document.getElementById("theme-id")) {
     themeId = document.createElement("style");
     themeId.setAttribute("id", "theme-id");
-    themeId.appendChild(
-      document.createTextNode(
-        `:root{
-            --color-border: hsl(235deg 16% 29%);
-            --color-disable:hsl(234deg 12% 48%);
-            --color-task: hsl(234, 39%, 85%);
-            --base-background: hsl(235, 24%, 19%);
-            --background-color-dark: hsl(235, 21%, 11%);
-            --background-image: url(../assets/imgs/bg-desktop-dark.jpg);
-        }`
-      )
-    );
-    document.head.appendChild(themeId);
+    if (isMobile) {
+      themeId.appendChild(
+        document.createTextNode(
+          `:root{
+          --color-border: hsl(235deg 16% 29%);
+          --color-disable:hsl(234deg 12% 48%);
+          --color-task: hsl(234, 39%, 85%);
+          --base-background: hsl(235, 24%, 19%);
+          --background-color-dark: hsl(235, 21%, 11%);
+          --background-image: url(bg-mobile-dark.jpg);
+      }`
+        )
+        )
+        return document.head.appendChild(themeId);
+    } else {
+       themeId.appendChild(
+        document.createTextNode(
+          `:root{
+                --color-border: hsl(235deg 16% 29%);
+                --color-disable:hsl(234deg 12% 48%);
+                --color-task: hsl(234, 39%, 85%);
+                --base-background: hsl(235, 24%, 19%);
+                --background-color-dark: hsl(235, 21%, 11%);
+                --background-image: url(bg-desktop-dark.jpg);
+            }`
+        )
+      );
+      return document.head.appendChild(themeId);
+    }
   }
 
   function defineDark() {
@@ -34,7 +52,23 @@ export function Header() {
         --color-task: hsl(234, 39%, 85%);
         --base-background: hsl(235, 24%, 19%);
         --background-color-dark: hsl(235, 21%, 11%);
-        --background-image: url(assets/imgs/bg-desktop-dark.jpg);
+        --background-image: url(bg-desktop-dark.jpg);
+    }`
+      )
+    );
+  }
+  function defineDarkMobile() {
+    setTheme("dark");
+    themeId.removeChild(themeId.firstChild);
+    themeId.appendChild(
+      document.createTextNode(
+        `:root{
+        --color-border: hsl(235deg 16% 29%);
+        --color-disable:hsl(234deg 12% 48%);
+        --color-task: hsl(234, 39%, 85%);
+        --base-background: hsl(235, 24%, 19%);
+        --background-color-dark: hsl(235, 21%, 11%);
+        --background-image: url(bg-mobile-dark.jpg);
     }`
       )
     );
@@ -50,7 +84,23 @@ export function Header() {
         --color-task: hsl(235, 19%, 35%);
         --base-background: hsl(0deg 0% 100%);
         --background-color-dark: hsl(0deg 0% 98%);
-        --background-image: url(assets/imgs/bg-desktop-light.jpg);
+        --background-image: url(bg-desktop-light.jpg);
+    }`
+      )
+    );
+  }
+  function defineLightMobile() {
+    setTheme("light");
+    themeId.removeChild(themeId.firstChild);
+    themeId.appendChild(
+      document.createTextNode(
+        `:root{
+        --color-border: hsl(233, 11%, 84%);
+        --color-disable:hsl(236, 9%, 61%);
+        --color-task: hsl(235, 19%, 35%);
+        --base-background: hsl(0deg 0% 100%);
+        --background-color-dark: hsl(0deg 0% 98%);
+        --background-image: url(bg-mobile-light.jpg);
     }`
       )
     );
@@ -65,9 +115,15 @@ export function Header() {
     <div className="header-container">
       <span className="header-todo">TODO</span>
       {theme === "light" ? (
-        <button className="dark" onClick={defineDark} />
+        <button
+          className="dark"
+          onClick={isMobile ? defineDarkMobile : defineDark}
+        />
       ) : (
-        <button className="light" onClick={defineLight} />
+        <button
+          className="light"
+          onClick={isMobile ? defineLightMobile : defineLight}
+        />
       )}
     </div>
   );
